b21ad1bdfdcb6c915961662487cbe92ac6152e94
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 19:20:02 +0300

    :runner: feat(#1): FindFavourite in `:weather:api`

--- rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/datasource/location/LocationRepository.kt
+++ rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/datasource/location/LocationRepository.kt
@@ -1,6 +1,10 @@
 package github.makeitvsolo.kweather.weather.api.datasource.location
 
 import github.makeitvsolo.kweather.weather.api.datasource.location.operation.AddFavourite
+import github.makeitvsolo.kweather.weather.api.datasource.location.operation.FindFavourite
 import github.makeitvsolo.kweather.weather.api.datasource.location.operation.RemoveFavourite
 
-interface LocationRepository : AddFavourite, RemoveFavourite
+interface LocationRepository :
+    AddFavourite,
+    RemoveFavourite,
+    FindFavourite
---
+++ rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/datasource/location/operation/FindFavourite.kt
@@ -0,0 +1,41 @@
+package github.makeitvsolo.kweather.weather.api.datasource.location.operation
+
+import github.makeitvsolo.kweather.core.error.handling.Result
+import github.makeitvsolo.kweather.core.mapping.Into
+import github.makeitvsolo.kweather.weather.domain.location.Location
+
+import java.math.BigDecimal
+
+interface MapFindFavouriteErrorInto<out R> : Into<R> {
+
+    fun fromNotFoundError(details: String): R
+    fun fromInternalError(throwable: Throwable): R
+}
+
+sealed interface FindFavouriteError {
+
+    fun <R, M : MapFindFavouriteErrorInto<R>> into(map: M): R
+
+    data class NotFoundError(private val details: String) : FindFavouriteError {
+
+        override fun <R, M : MapFindFavouriteErrorInto<R>> into(map: M): R =
+            map.fromNotFoundError(details)
+    }
+
+    data class InternalError(private val throwable: Throwable) : FindFavouriteError {
+
+        override fun <R, M : MapFindFavouriteErrorInto<R>> into(map: M): R =
+            map.fromInternalError(throwable)
+    }
+}
+
+interface FindFavourite {
+
+    fun findAllFavourite(accountId: String): Result<List<Location>, FindFavouriteError>
+
+    fun findFavourite(
+        accountId: String,
+        latitude: BigDecimal,
+        longitude: BigDecimal
+    ): Result<Location, FindFavouriteError>
+}
