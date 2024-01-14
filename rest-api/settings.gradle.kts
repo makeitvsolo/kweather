b79611af6cb976ce958d41211f61cd3310dcaf82
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 20:37:22 +0300

    :hammer: build: add `:user-access:domain` mod

--- rest-api/settings.gradle.kts
+++ rest-api/settings.gradle.kts
@@ -5,3 +5,6 @@ include("internal:core")
 include("internal:weather")
 include("internal:weather:domain")
 include("internal:weather:api")
+
+include("internal:user-access")
+include("internal:user-access:domain")
