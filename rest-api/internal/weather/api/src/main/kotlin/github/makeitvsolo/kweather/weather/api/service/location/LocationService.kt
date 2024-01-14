9c91c19ef85a743223fa856ad5280333c462c8a9
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 20:05:29 +0300

    :wrench: refactor: SaveToFavourites in `:weather:api`

--- rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/service/location/LocationService.kt
+++ rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/service/location/LocationService.kt
@@ -1,12 +1,12 @@
 package github.makeitvsolo.kweather.weather.api.service.location
 
-import github.makeitvsolo.kweather.weather.api.service.location.usecase.AddFavourite
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.FetchFavourite
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.RemoveFavourite
+import github.makeitvsolo.kweather.weather.api.service.location.usecase.SaveToFavourites
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.SearchLocation
 
 interface LocationService :
-    AddFavourite,
+    SaveToFavourites,
     RemoveFavourite,
     FetchFavourite,
     SearchLocation
