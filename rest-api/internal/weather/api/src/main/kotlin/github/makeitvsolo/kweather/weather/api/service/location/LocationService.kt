3858aeecf559597959ba378bb06331530204947a
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 20:08:14 +0300

    :wrench: refactor: RemoveFromFavourites in `:weather:api`

--- rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/service/location/LocationService.kt
+++ rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/service/location/LocationService.kt
@@ -1,12 +1,12 @@
 package github.makeitvsolo.kweather.weather.api.service.location
 
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.FetchFavourite
-import github.makeitvsolo.kweather.weather.api.service.location.usecase.RemoveFavourite
+import github.makeitvsolo.kweather.weather.api.service.location.usecase.RemoveFromFavourites
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.SaveToFavourites
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.SearchLocation
 
 interface LocationService :
     SaveToFavourites,
-    RemoveFavourite,
+    RemoveFromFavourites,
     FetchFavourite,
     SearchLocation
---
+++ rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/service/location/usecase/RemoveFromFavourites.kt
@@ -0,0 +1,45 @@
+package github.makeitvsolo.kweather.weather.api.service.location.usecase
+
+import github.makeitvsolo.kweather.core.error.handling.Result
+import github.makeitvsolo.kweather.core.mapping.Into
+import github.makeitvsolo.kweather.weather.api.datasource.location.operation.MapRemoveFavouriteErrorInto
+import github.makeitvsolo.kweather.weather.api.service.location.dto.FavouriteLocationDto
+
+typealias RemoveFromFavouritesPayload = FavouriteLocationDto
+
+interface MapRemoveFromFavouritesErrorInto<out R> : Into<R> {
+
+    fun fromNotFoundError(details: String): R
+    fun fromInternalError(throwable: Throwable): R
+}
+
+sealed interface RemoveFromFavouritesError {
+
+    object FromRemoveFavouriteError : MapRemoveFavouriteErrorInto<RemoveFromFavouritesError> {
+
+        override fun fromNotFoundError(details: String): RemoveFromFavouritesError =
+            NotFoundError(details)
+
+        override fun fromInternalError(throwable: Throwable): RemoveFromFavouritesError =
+            InternalError(throwable)
+    }
+
+    fun <R, M : MapRemoveFromFavouritesErrorInto<R>> into(map: M): R
+
+    data class NotFoundError(private val details: String) : RemoveFromFavouritesError {
+
+        override fun <R, M : MapRemoveFromFavouritesErrorInto<R>> into(map: M): R =
+            map.fromNotFoundError(details)
+    }
+
+    data class InternalError(private val throwable: Throwable) : RemoveFromFavouritesError {
+
+        override fun <R, M : MapRemoveFromFavouritesErrorInto<R>> into(map: M): R =
+            map.fromInternalError(throwable)
+    }
+}
+
+interface RemoveFromFavourites {
+
+    fun removeFromFavourites(payload: RemoveFromFavouritesPayload): Result<Unit, RemoveFromFavouritesError>
+}
