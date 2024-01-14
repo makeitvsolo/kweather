3718bd07842b47bb93eb627ae68d78d3d0eccf69
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 21:41:48 +0300

    :wrench: fix: ActiveUserDto mapping in `:user-access:api`

--- rest-api/internal/user-access/api/src/main/kotlin/github/makeitvsolo/kweather/user/access/api/service/dto/ActiveUserDto.kt
+++ rest-api/internal/user-access/api/src/main/kotlin/github/makeitvsolo/kweather/user/access/api/service/dto/ActiveUserDto.kt
@@ -1,6 +1,15 @@
 package github.makeitvsolo.kweather.user.access.api.service.dto
 
+import github.makeitvsolo.kweather.user.access.domain.MapUserInto
+
 data class ActiveUserDto(
     val id: String,
     val name: String
-)
+) {
+
+    object FromUser : MapUserInto<ActiveUserDto> {
+
+        override fun from(id: String, name: String): ActiveUserDto =
+            ActiveUserDto(id, name)
+    }
+}
