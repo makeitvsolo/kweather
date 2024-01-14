43e0b1221c53aec6f36e80f98f883075e55f15bc
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 20:52:52 +0300

    :runner: feat(#1): impl User in `:user-access:domain`

---
+++ rest-api/internal/user-access/domain/src/main/kotlin/github/makeitvsolo/kweather/user/access/domain/User.kt
@@ -0,0 +1,43 @@
+package github.makeitvsolo.kweather.user.access.domain
+
+import github.makeitvsolo.kweather.core.mapping.Into
+import github.makeitvsolo.kweather.core.type.Unique
+
+interface MapUserInto<out R> : Into<R> {
+
+    fun from(id: String, name: String): R
+}
+
+class User internal constructor(
+    private val id: String,
+    private val name: String,
+    private val password: String
+) {
+
+    fun isValidCredentials(name: String, password: String): Boolean =
+        this.password == password && this.name == name
+
+    fun <R, M : MapUserInto<R>> into(map: M): R =
+        map.from(id, name)
+
+    override fun equals(other: Any?): Boolean = (other is User) && id == other.id
+
+    override fun hashCode(): Int = id.hashCode()
+
+    companion object {
+
+        fun from(
+            id: String,
+            name: String,
+            password: String
+        ): User =
+            User(id, name, password)
+
+        fun create(
+            id: Unique<String>,
+            name: String,
+            password: String
+        ): User =
+            User(id.unique(), name, password)
+    }
+}
