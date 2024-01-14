6d064552aa481b0ff11f6f8ff5e1bfbe9886f35d
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 21:11:12 +0300

    :runner: feat(#1): FindUser in `:user-access:api`

---
+++ rest-api/internal/user-access/api/src/main/kotlin/github/makeitvsolo/kweather/user/access/api/datasource/UserRepository.kt
@@ -0,0 +1,5 @@
+package github.makeitvsolo.kweather.user.access.api.datasource
+
+import github.makeitvsolo.kweather.user.access.api.datasource.operation.FindUser
+
+interface UserRepository : FindUser
---
+++ rest-api/internal/user-access/api/src/main/kotlin/github/makeitvsolo/kweather/user/access/api/datasource/operation/FindUser.kt
@@ -0,0 +1,33 @@
+package github.makeitvsolo.kweather.user.access.api.datasource.operation
+
+import github.makeitvsolo.kweather.core.error.handling.Result
+import github.makeitvsolo.kweather.core.mapping.Into
+import github.makeitvsolo.kweather.user.access.domain.User
+
+interface MapFindUserErrorInto<out R> : Into<R> {
+
+    fun fromNotFoundError(details: String): R
+    fun fromInternalError(throwable: Throwable): R
+}
+
+sealed interface FindUserError {
+
+    fun <R, M : MapFindUserErrorInto<R>> into(map: M): R
+
+    data class NotFoundError(private val details: String) : FindUserError {
+
+        override fun <R, M : MapFindUserErrorInto<R>> into(map: M): R =
+            map.fromNotFoundError(details)
+    }
+
+    data class InternalError(private val throwable: Throwable) : FindUserError {
+
+        override fun <R, M : MapFindUserErrorInto<R>> into(map: M): R =
+            map.fromInternalError(throwable)
+    }
+}
+
+interface FindUser {
+
+    fun findByName(name: String): Result<User, FindUserError>
+}
