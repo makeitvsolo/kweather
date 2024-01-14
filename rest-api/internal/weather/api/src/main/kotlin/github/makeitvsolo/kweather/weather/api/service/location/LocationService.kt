45fc1c878b882b05a740efef6dd203ec07af2733
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-14 20:10:38 +0300

    :wrench: refactor: SearchForLocation in `:weather:api`

--- rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/service/location/LocationService.kt
+++ rest-api/internal/weather/api/src/main/kotlin/github/makeitvsolo/kweather/weather/api/service/location/LocationService.kt
@@ -3,10 +3,10 @@ package github.makeitvsolo.kweather.weather.api.service.location
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.FetchFavourite
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.RemoveFromFavourites
 import github.makeitvsolo.kweather.weather.api.service.location.usecase.SaveToFavourites
-import github.makeitvsolo.kweather.weather.api.service.location.usecase.SearchLocation
+import github.makeitvsolo.kweather.weather.api.service.location.usecase.SearchForLocation
 
 interface LocationService :
     SaveToFavourites,
     RemoveFromFavourites,
     FetchFavourite,
-    SearchLocation
+    SearchForLocation
