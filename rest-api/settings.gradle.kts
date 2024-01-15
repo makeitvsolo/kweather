62b2542541e56920e67d23c7ca014e4af68d7966
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-15 11:32:14 +0300

    :hammer: build: add test dependencies to `:user-access:application` mod

--- rest-api/settings.gradle.kts
+++ rest-api/settings.gradle.kts
@@ -10,3 +10,16 @@ include("internal:user-access")
 include("internal:user-access:domain")
 include("internal:user-access:api")
 include("internal:user-access:application")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("testWorkspace") {
+            val kotlinTest = version("kotlin-test", "1.9.21")
+            val mockito = version("mockito", "5.2.1")
+
+            library("kotlin-test", "org.jetbrains.kotlin", "kotlin-test").versionRef(kotlinTest)
+
+            library("mockito-kotlin", "org.mockito.kotlin", "mockito-kotlin").versionRef(mockito)
+        }
+    }
+}
