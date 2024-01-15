bd88ee03ac2a88e89f72243ad44f5e5ded9135da
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-15 16:55:51 +0300

    :runner: feat(#1): impl ConfigureSqlUserRepository in `:user-access:infrastructure`

---
+++ rest-api/internal/user-access/infrastructure/src/main/kotlin/github/makeitvsolo/kweather/user/access/infrastructure/datasource/configure/ConfigureSqlUserRepository.kt
@@ -0,0 +1,59 @@
+package github.makeitvsolo.kweather.user.access.infrastructure.datasource.configure
+
+import github.makeitvsolo.kweather.core.error.handling.Result
+import github.makeitvsolo.kweather.user.access.infrastructure.datasource.SqlUserRepository
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+
+sealed interface SqlUserRepositoryConfigurationError {
+
+    data class DataSourceUrlError(private val details: String) : SqlUserRepositoryConfigurationError
+
+    data class InvalidCredentialsError(private val details: String) : SqlUserRepositoryConfigurationError
+}
+
+class ConfigureSqlUserRepository internal constructor(
+    private var url: String? = null,
+    private var username: String? = null,
+    private var password: String? = null
+) {
+
+    fun datasourceUrl(url: String) = apply { this.url = url }
+    fun username(username: String) = apply { this.username = username }
+    fun password(password: String) = apply { this.password = password }
+
+    fun configured(): Result<SqlUserRepository, SqlUserRepositoryConfigurationError> {
+        val appliedUrl = url
+        val appliedUsername = username
+        val appliedPassword = password
+
+        appliedUrl ?: return Result.error(
+            SqlUserRepositoryConfigurationError.DataSourceUrlError("missing datasource url")
+        )
+
+        appliedUsername ?: return Result.error(
+            SqlUserRepositoryConfigurationError.InvalidCredentialsError("missing username")
+        )
+
+        appliedPassword ?: return Result.error(
+            SqlUserRepositoryConfigurationError.InvalidCredentialsError("missing password")
+        )
+
+        val config = HikariConfig().apply {
+            jdbcUrl = appliedUrl
+            username = appliedUsername
+            password = appliedPassword
+        }
+
+        val datasource = HikariDataSource(config)
+        return Result.ok(
+            SqlUserRepository(datasource)
+        )
+    }
+
+    companion object {
+
+        fun with(): ConfigureSqlUserRepository = ConfigureSqlUserRepository()
+    }
+}
