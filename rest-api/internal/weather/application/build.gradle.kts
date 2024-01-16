f2aaaf0895beb83180965d3974174d9a622bf5b7
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-16 12:10:55 +0300

    :hammer: build: add test dependencies to `:weather:application` mod

--- rest-api/internal/weather/application/build.gradle.kts
+++ rest-api/internal/weather/application/build.gradle.kts
@@ -2,4 +2,7 @@ dependencies {
     implementation(project(":internal:core"))
     implementation(project(":internal:weather:domain"))
     implementation(project(":internal:weather:api"))
+
+    testImplementation(testWorkspace.kotlin.test)
+    testImplementation(testWorkspace.mockito.kotlin)
 }
