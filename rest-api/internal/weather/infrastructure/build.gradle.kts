f3a27b6705dcff7d5e07e9237d8a24521e08b059
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-16 14:21:16 +0300

    :hammer: build: add postgres dependencies to `:weather:infrastructure` mod

--- rest-api/internal/weather/infrastructure/build.gradle.kts
+++ rest-api/internal/weather/infrastructure/build.gradle.kts
@@ -2,4 +2,7 @@ dependencies {
     implementation(project(":internal:core"))
     implementation(project(":internal:weather:domain"))
     implementation(project(":internal:weather:api"))
+
+    implementation(workspace.datasource.postgres)
+    implementation(workspace.datasource.hikari)
 }
