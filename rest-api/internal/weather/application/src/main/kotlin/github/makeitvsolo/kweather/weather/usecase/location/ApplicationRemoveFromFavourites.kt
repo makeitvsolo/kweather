0547d6a756c01ee953e4f1330e0b0ef885f8c5c4
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-16 11:46:45 +0300

    :runner: feat(#1): impl ApplicationRemoveFromFavourites in `:weather:application`

---
+++ rest-api/internal/weather/application/src/main/kotlin/github/makeitvsolo/kweather/weather/usecase/location/ApplicationRemoveFromFavourites.kt
@@ -0,0 +1,17 @@
+package github.makeitvsolo.kweather.weather.usecase.location
+
+import github.makeitvsolo.kweather.core.error.handling.Result
+import github.makeitvsolo.kweather.weather.api.datasource.location.LocationRepository
+import github.makeitvsolo.kweather.weather.api.service.location.usecase.RemoveFromFavourites
+import github.makeitvsolo.kweather.weather.api.service.location.usecase.RemoveFromFavouritesError
+import github.makeitvsolo.kweather.weather.api.service.location.usecase.RemoveFromFavouritesPayload
+
+class ApplicationRemoveFromFavourites(
+    private val repository: LocationRepository
+) : RemoveFromFavourites {
+
+    override fun removeFromFavourites(payload: RemoveFromFavouritesPayload): Result<Unit, RemoveFromFavouritesError> =
+        repository.removeFavourite(payload.accountId, payload.latitude, payload.longitude).mapError { error ->
+            error.into(RemoveFromFavouritesError.FromRemoveFavouriteError)
+        }
+}
