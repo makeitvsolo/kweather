31f839238bc08d35685dbab49230ced1bb4563f9
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-16 11:58:01 +0300

    :runner: feat(#1): impl ApplicationFetchWeather in `:weather:application`

---
+++ rest-api/internal/weather/application/src/main/kotlin/github/makeitvsolo/kweather/weather/usecase/weather/ApplicationFetchWeather.kt
@@ -0,0 +1,32 @@
+package github.makeitvsolo.kweather.weather.usecase.weather
+
+import github.makeitvsolo.kweather.core.error.handling.Result
+import github.makeitvsolo.kweather.weather.api.datasource.location.LocationRepository
+import github.makeitvsolo.kweather.weather.api.datasource.weather.WeatherRepository
+import github.makeitvsolo.kweather.weather.api.service.weather.dto.CurrentWeatherDto
+import github.makeitvsolo.kweather.weather.api.service.weather.dto.LocationDto
+import github.makeitvsolo.kweather.weather.api.service.weather.dto.WeatherDto
+import github.makeitvsolo.kweather.weather.api.service.weather.usecase.FetchWeather
+import github.makeitvsolo.kweather.weather.api.service.weather.usecase.FetchWeatherError
+import github.makeitvsolo.kweather.weather.api.service.weather.usecase.FetchWeatherPayload
+import github.makeitvsolo.kweather.weather.api.service.weather.usecase.FetchWeatherResponse
+
+class ApplicationFetchWeather(
+    private val locationRepository: LocationRepository,
+    private val weatherRepository: WeatherRepository
+) : FetchWeather {
+
+    override fun fetchWeather(payload: FetchWeatherPayload): Result<FetchWeatherResponse, FetchWeatherError> =
+        locationRepository.findFavourite(payload.accountId, payload.latitude, payload.longitude).mapError { error ->
+            error.into(FetchWeatherError.FromFindFavouriteError)
+        }.andThen { location ->
+            weatherRepository.findByCoordinates(payload.latitude, payload.longitude).mapError { error ->
+                error.into(FetchWeatherError.FromFindWeatherError)
+            }.map { weather ->
+                CurrentWeatherDto(
+                    location.into(LocationDto.FromLocation),
+                    weather.into(WeatherDto.FromWeather)
+                )
+            }
+        }
+}
