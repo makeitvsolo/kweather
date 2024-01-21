343efc4e4ceb766f6f2188db3c5a54217fbbb21f
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-21 12:31:10 +0300

    :wrench: refactor: WeatherApiWeatherRepository in `:weather:infrastructure`

--- rest-api/internal/weather/infrastructure/src/main/kotlin/github/makeitvsolo/kweather/weather/infrastructure/datasource/weather/weatherapi/WeatherApiWeatherRepository.kt
+++ rest-api/internal/weather/infrastructure/src/main/kotlin/github/makeitvsolo/kweather/weather/infrastructure/datasource/weather/weatherapi/WeatherApiWeatherRepository.kt
@@ -5,17 +5,7 @@ import github.makeitvsolo.kweather.weather.api.datasource.weather.WeatherReposit
 import github.makeitvsolo.kweather.weather.api.datasource.weather.error.FindForecastError
 import github.makeitvsolo.kweather.weather.api.datasource.weather.error.FindWeatherError
 import github.makeitvsolo.kweather.weather.domain.weather.current.Weather
-import github.makeitvsolo.kweather.weather.domain.weather.current.value.Cloudiness
-import github.makeitvsolo.kweather.weather.domain.weather.current.value.Humidity
-import github.makeitvsolo.kweather.weather.domain.weather.current.value.Precipitation
-import github.makeitvsolo.kweather.weather.domain.weather.current.value.Pressure
-import github.makeitvsolo.kweather.weather.domain.weather.current.value.Temperature
-import github.makeitvsolo.kweather.weather.domain.weather.current.value.Wind
 import github.makeitvsolo.kweather.weather.domain.weather.forecast.DailyWeather
-import github.makeitvsolo.kweather.weather.domain.weather.forecast.value.DailyHumidity
-import github.makeitvsolo.kweather.weather.domain.weather.forecast.value.DailyPrecipitation
-import github.makeitvsolo.kweather.weather.domain.weather.forecast.value.DailyTemperature
-import github.makeitvsolo.kweather.weather.domain.weather.forecast.value.DailyWind
 import github.makeitvsolo.kweather.weather.infrastructure.datasource.weather.weatherapi.data.WeatherApiForecast
 import github.makeitvsolo.kweather.weather.infrastructure.datasource.weather.weatherapi.data.WeatherApiWeather
 
@@ -25,9 +15,6 @@ import com.github.kittinunf.result.Result.Failure
 import com.github.kittinunf.result.Result.Success
 
 import java.math.BigDecimal
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class WeatherApiWeatherRepository internal constructor(
     private val apiKey: String,
@@ -48,22 +35,7 @@ class WeatherApiWeatherRepository internal constructor(
                 val response = result.get()
 
                 Result.ok(
-                    Weather.from(
-                        response.current.condition.code,
-                        response.current.condition.summary,
-                        LocalDateTime.parse(response.location.localtime, DateTimeFormatter.ofPattern(DATETIME_FORMAT)),
-                        Temperature(response.current.temperature, response.current.feelsLike),
-                        Wind(
-                            response.current.windSpeed,
-                            response.current.windGust,
-                            response.current.windDegree,
-                            response.current.windDirection
-                        ),
-                        Pressure(response.current.pressure),
-                        Humidity(response.current.humidity),
-                        Cloudiness(response.current.cloud),
-                        Precipitation(response.current.precipitation)
-                    )
+                    response.intoWeather()
                 )
             }
 
@@ -89,25 +61,7 @@ class WeatherApiWeatherRepository internal constructor(
                 val response = result.get()
 
                 Result.ok(
-                    response.forecast.days.map { day ->
-                        DailyWeather.from(
-                            day.weather.condition.code,
-                            day.weather.condition.summary,
-                            LocalDate.parse(day.date, DateTimeFormatter.ofPattern(DATE_FORMAT)),
-                            DailyTemperature(
-                                day.weather.averageTemperature,
-                                day.weather.maxTemperature,
-                                day.weather.minTemperature
-                            ),
-                            DailyWind(day.weather.wind),
-                            DailyHumidity(day.weather.humidity),
-                            DailyPrecipitation(
-                                day.weather.precipitation,
-                                day.weather.rainChance,
-                                day.weather.snowChance
-                            )
-                        )
-                    }
+                    response.intoForecast()
                 )
             }
 
@@ -116,9 +70,6 @@ class WeatherApiWeatherRepository internal constructor(
     }
 
     companion object {
-
-        private const val DATETIME_FORMAT = "yyyy-MM-dd HH:mm"
-        private const val DATE_FORMAT = "yyyy-MM-dd"
 
         private const val FORECAST_API_URL = "http://api.weatherapi.com/v1/forecast.json"
         private const val FORECAST_DAYS_PARAMETER = "days"
