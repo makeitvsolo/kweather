a4be2cb11ae4e02e5a6257005d849fd222fe55aa
makeitvsolo <vane4ka.dev@gmail.com>
2024-01-22 19:44:05 +0300

    :runner: feat(#1): impl WeatherServiceConfiguration in `:boot`

---
+++ rest-api/boot/src/main/kotlin/github/makeitvsolo/kweather/boot/configuration/weather/service/WeatherServiceConfiguration.kt
@@ -0,0 +1,38 @@
+package github.makeitvsolo.kweather.boot.configuration.weather.service
+
+import github.makeitvsolo.kweather.weather.api.datasource.location.LocationRepository
+import github.makeitvsolo.kweather.weather.api.datasource.weather.WeatherRepository
+import github.makeitvsolo.kweather.weather.api.service.weather.WeatherService
+import github.makeitvsolo.kweather.weather.api.service.weather.usecase.FetchForecast
+import github.makeitvsolo.kweather.weather.api.service.weather.usecase.FetchWeather
+import github.makeitvsolo.kweather.weather.application.weather.ApplicationWeatherService
+import github.makeitvsolo.kweather.weather.application.weather.usecase.ApplicationFetchForecast
+import github.makeitvsolo.kweather.weather.application.weather.usecase.ApplicationFetchWeather
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class WeatherServiceConfiguration {
+
+    @Bean
+    fun fetchWeatherUsecase(
+        locationRepository: LocationRepository,
+        weatherRepository: WeatherRepository
+    ): FetchWeather =
+        ApplicationFetchWeather(locationRepository, weatherRepository)
+
+    @Bean
+    fun fetchForecastUsecase(
+        locationRepository: LocationRepository,
+        weatherRepository: WeatherRepository
+    ): FetchForecast =
+        ApplicationFetchForecast(locationRepository, weatherRepository)
+
+    @Bean
+    fun weatherService(
+        fetchWeatherUsecase: FetchWeather,
+        fetchForecastUsecase: FetchForecast
+    ): WeatherService =
+        ApplicationWeatherService(fetchWeatherUsecase, fetchForecastUsecase)
+}
